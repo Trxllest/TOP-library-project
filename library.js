dc2298e0302e054c81736b4387afdc3d4589a9c9
David Olona <damilear@outlook.com>
2023-10-16 20:17:23 -0400

    Coomplete

--- library.js
+++ library.js
@@ -1,10 +1,7 @@
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 const harryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 320, true);
 
-// console.log(theHobbit.info()); // Output: "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-// console.log(harryPotter.info());
-
-const myLibrary = [theHobbit, harryPotter];
+const myLibrary = [];
 const contentArea = document.querySelector('.project-cards');
 const newBookBtn = document.querySelector('.hbtn');
 const dialogBox = document.getElementById('bookDialog');
@@ -26,38 +23,61 @@ function Book(title, author, pages, read = false) {
     }  
 }
 
-function CreateCard() {
+function CreateCard(book, index) {
     // .project-card>p.title+proj-content
     let card = document.createElement('div');
     card.classList.add('project-card');
     contentArea.appendChild(card);
+
     let cTitle = document.createElement('p');
     cTitle.classList.add('title');
     card.appendChild(cTitle);
+    cTitle.innerText = book.title;
+
     let cContent = document.createElement('div');
     cContent.classList.add('proj-content');
     card.appendChild(cContent);
+    cContent.innerText = book.info();
 
-    // cTitle.textContent = 'testwsdgsgnasC'
+    let readToggle = document.createElement('button');
+    readToggle.textContent = 'Read';
+    readToggle.addEventListener('click', () => {
+        book.read ? book.read = false : book.read = true;
+        cContent.innerText = book.info();
+    });
+    card.appendChild(readToggle);
+
+    let deleteBook = document.createElement('button');
+    deleteBook.textContent = 'Remove';
+    deleteBook.addEventListener('click', () => {
+        // Remove the book from the library and the DOM
+        myLibrary.splice(index, 1);
+        contentArea.removeChild(card);
+        console.log(myLibrary);
+    });
+    card.appendChild(deleteBook);
 }
 
 function AddBook(book) {
     myLibrary.push(book)
-    CreateCard();
-    let card = contentArea.lastElementChild;
-    let title = card.firstElementChild;
-    let text = card.lastElementChild;
-    title.textContent = book.title;
-    text.textContent = book.info();
+    CreateCard(book, myLibrary.length - 1);
 }
 
-function CheckBooks(library) {
-    library.forEach(AddBook);
-}
 
 newBookBtn.addEventListener('click', () => {
     dialogBox.showModal();
 });
+
+
+const errorMessage = document.getElementById('error-message');
+
+function clearErrorMessage() {
+    errorMessage.textContent = '';
+}
+
+function displayErrorMessage(message) {
+    errorMessage.textContent = message;
+}
 
 addButton.addEventListener('click', (e) => {
     e.preventDefault();
@@ -80,5 +100,3 @@ addButton.addEventListener('click', (e) => {
 cancelAdd.addEventListener('click', () => {
     dialogBox.close();
 });
-
-document.addEventListener("DOMContentLoaded", CheckBooks(myLibrary));
